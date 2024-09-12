24a693b909370ce2ee39707f6486bb57efda242b
Беляева Валерия Андреевна <62827928+ValeriaBelyaeva@users.noreply.github.com>
2024-09-12 23:53:55 +0700

    fix bag (whith 0)

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ n = int(fl.readline())
 fl.close()
 
 fl = open("output", 'w')
-fib = [0 for i in range(n+1)]
+fib = [0 for i in range(n+2)]
 fib[1] = 1
 for i in range(2, n+1):
     fib[i] = fib[i-1]+fib[i-2]
